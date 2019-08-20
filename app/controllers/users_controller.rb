5e30e18f64e7e3df8506e20d04ff80a1cceb5c5a
takedat-t <dktthkt@gmail.com>
2019-08-20 10:07:13 +0000

    10.2まで add-user-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+  @first_day = Date.current.beginning_of_month
+  @last_day = @first_day.end_of_month
   end
 
   def new
